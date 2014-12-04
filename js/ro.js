18cb243cf84c3c3d270fb03413bb7b3d3127c6ed
Niimi <yuki_nimi@waku-2.com>
2014-12-04 19:01:09 +0900

    微修正

--- js/ro.js
+++ js/ro.js
@@ -1,33 +1,36 @@
-// JavaScript Document
-// UTF-8
+// RollOver Script
+// 自由設定
+var key_class = 'hover'; // Class名
+var add_rollover = '_h'; // ファイル名に追加される文字 test.jpg → test_h.jpg
+
+// core
+function rolloverJs(){
+	var imgDom = $('img.'+key_class+',.'+key_class+' img');
+	imgDom.unbind("mouseenter").unbind("mouseleave"); // 重複処理を避けるためunbindを行う
+	// 先読み
+	for (i=0; i < imgDom.length ;i++){
+		var imgObj = new Image();
+		imagePath = imgDom.eq(i).attr('src');
+		imgDom.eq(i).data('mouse_off',imagePath);
+		imagePath_sp = imagePath.split('.'); 
+		imagePath_sp[imagePath_sp.length - 2] = imagePath_sp[imagePath_sp.length - 2] + add_rollover;
+		imagePath = imagePath_sp.join('.');
+		imgDom.eq(i).data('mouse_on',imagePath);
+		imgObj.src = imagePath;
+	};
+	// マウス オンオフ
+	imgDom.hover(function(){
+		$(this).attr('src',$(this).data('mouse_on'));
+	},function(){
+		$(this).attr('src',$(this).data('mouse_off'));
+	});
+};
 
 (function($){
 	$(document).ready(function(){
 		
-		// RollOver Script
-		var key_class = 'hover';
-		var add_rollover = '_h';
-		
-		// 先読み
-		for (i=0; i < $('img.'+key_class).length ;i++){
-			var imgObj = new Image();
-			imagePath = $('img.'+key_class).eq(i).attr('src');
-			$('img.'+key_class).eq(i).data('mouse_off',imagePath);
-			imagePath_sp = imagePath.split('.'); 
-			imagePath_sp[imagePath_sp.length - 2] = imagePath_sp[imagePath_sp.length - 2] + add_rollover;
-			imagePath = imagePath_sp.join('.');
-			$('img.'+key_class).eq(i).data('mouse_on',imagePath);
-			imgObj.src = imagePath;
-		};
-		// マウス オンオフ
-		$('img.'+key_class).hover(function(){
-			$(this).attr('src',$(this).data('mouse_on'));
-		},function(){
-			$(this).attr('src',$(this).data('mouse_off'));
-		});
-		
+		rolloverJs();
 
 	});
 })(jQuery);
-
 
